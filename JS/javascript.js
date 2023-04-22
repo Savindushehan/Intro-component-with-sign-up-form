44855768905fff3bad99e478b443a9de791001b3
Savindushehan <savindushehansamaraweera@gmail.com>
2023-04-22 11:31:32 +0530

    first upload

---
+++ JS/javascript.js
@@ -0,0 +1,31 @@
+console.clear();
+
+var Firstname=document.getElementById("firstname");
+var Lastname=document.getElementById("lastname");
+var Email=document.getElementById("email");
+var Password=document.getElementById("password");
+var pattern=/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
+var Submit=document.getElementById("submit-btn");
+var Image1=document.getElementById("image1");
+var Image2=document.getElementById("image2");
+var Image3=document.getElementById("image3");
+var Image4=document.getElementById("image4");
+var Error1=document.getElementById("error1");
+var Error2=document.getElementById("error2");
+var Error3=document.getElementById("error3");
+var Error4=document.getElementById("error4");
+
+
+Submit.onclick=function(){
+    if(!Email.value.match(pattern)){
+        Image1.style.display="block";
+        Image2.style.display="block";
+        Image3.style.display="block";
+        Image4.style.display="block";
+        Error1.style.display="block";
+        Error2.style.display="block";
+        Error3.style.display="block";
+        Error4.style.display="block";
+    }
+   
+}
